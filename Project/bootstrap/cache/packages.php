bfb69640926583ac2547a3dfcfeb90582025008b
tungntd00605 <tungntd00605@fpt.edu.vn>
2018-09-04 19:47:57 +0700

    Khởi tạo Project FPT Canteen

---
+++ Project/bootstrap/cache/packages.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'jrm2k6/cloudder' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'JD\\Cloudder\\CloudderServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Cloudder' => 'JD\\Cloudder\\Facades\\Cloudder',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+);
