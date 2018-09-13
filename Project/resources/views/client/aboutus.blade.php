d0a6050e54a2dc2aca5db704f8a9da189b1ac273
congdoan176 <doanncd00530@fpt.edu.vn>
2018-09-13 18:44:08 +0700

    Tạo mới trang About us.

---
+++ Project/resources/views/client/aboutus.blade.php
@@ -0,0 +1,28 @@
+@extends('layout.client',[
+    'page_title'=>'About us | Giới thiệu',
+    'current_page'=>'About_us',
+])
+
+@section('content')
+    <div class="container mt-5 pt-3 mt-5 py-5 col-md-8">
+        <h2><a href="/">Canteen-Fpt</a></h2>
+        <img src="https://ashikubo.net/wp-content/uploads/2018/01/cac-mon-vat-de-lam-de-ban-1.jpg" alt="" class="img-fluid" style="width: 100%;height: 350px">
+        <p class="text-primary pt-2"><strong>Chào các bạn đến với canteen của chúng tôi!</strong></p>
+        <p>Canteen chúng tôi là một cửa hàng nhỏ phục vụ ăn uống trong trường học.</p>
+        <p>&nbsp; Chúng tôi hằng ngày vẫn phục vụ tất cả mọi người trong trường học, từ 6 giờ sáng đến 7 giờ tối,
+        chúng tôi phục vụ đồ ăn và đồ uống cho mọi người theo yêu cầu. Đến nay chúng tôi không ngừng duy trì và phát triển cửa hàng,
+        chúng tôi đã thấy được tầm ảnh hưởng của internet đến cuộc sống hiện tại, để tiện lợi và phục vụ mọi người tốt nhất
+        có thể chúng tôi đưa và áp dụng internet vào cho cửa hàng. Bạn có thể gọi đồ online thông qua <a href="/">website</a> của chúng tôi là chúng tôi
+        đã có thể phục vụ bạn mà bạn không cần phải đến trực tiếp cửa hàng, chúng tôi sẽ có người đưa tới cho bạn với những sản phẩm
+        bạn yêu cầu tốt nhất cùng sự phục vụ nhiệt tình đến từ nhân viên của canteen.
+        Từ khi đưa <a href="/">website</a> vào, chúng tôi đã nhận được nhiều điều tích cực từ người dùng,chúng tôi nay đã có thể
+        phục vụ mọi người ngay khi mọi người không trực tiếp đến cửa hàng.</p>
+        <p><strong>Hãy đến với chúng tôi <a href="/">Canteen-fpt</a> để chúng tôi có thể phục vụ bạn.</strong></p>
+        <p>Cảm ơn các bạn đã đến canteen của chúng tôi!</p>
+        <p><strong>Mọi thông tin cần giải đáp và những góp ý cho chúng tôi, bạn có thể liên hệ với chúng tôi.</strong></p>
+        <p><a href="/">Canteen-fpt</a> tòa nhà Detech số 8, Tôn Thất Thuyết, Cầu Giấy, Hà Nội</p>
+        <p>Hotline: + 0166 807 7320.</p>
+        <p><strong>Email : </strong>canteenfpt@gmail.com</p>
+        <p><strong>Website:</strong><a href="/">Canteen-fpt.</a></p>
+    </div>
+@endsection
